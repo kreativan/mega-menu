7edebb1b3d6805a906af905198d7ab643ab8b8de
Ivan Milincic <lokomotivan@gmail.com>
2017-03-06 00:40:13 +0100

    update

---
+++ js/ivm.mega-menu.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+
+    $("#ivm-mega-menu-items").menuAim({
+        activate: function(a){
+            var idx = $(a).index();
+            $('.ivm-mega-menu-panel').eq(idx).show();
+            $("#ivm-mega-menu-items > li").eq(idx).addClass('ivm-hover');
+        },  // fired on row activation
+        deactivate: function(a){
+            var idx = $(a).index();
+            $('.ivm-mega-menu-panel').eq(idx).hide();
+            $("#ivm-mega-menu-items > li").eq(idx).removeClass('ivm-hover');
+        }  // fired on row deactivation
+    });
+
+    var ivm_mode = $('#ivm-mega-menu-button').data("menu-mode");
+
+    if(ivm_mode == "hover") {
+        $('#ivm-mega-menu-button, #ivm-mega-menu-wrapper, #ivm-mega-menu').hover(function() {
+            $('#ivm-mega-menu-wrapper').show();
+        }, function() {
+            $('#ivm-mega-menu-wrapper').hide();
+        });
+    }else if (ivm_mode == "click") {
+        $('#ivm-mega-menu-button').click(function() {
+            $('#ivm-mega-menu-wrapper').toggleClass('ivm-active');
+            $(this).toggleClass('ivm-active');
+        });
+    }
+
+});
